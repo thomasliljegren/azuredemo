9d415e4ea5b7d26d3a90accbd49bbcb0daea89af
Thomas Liljegren <thliljegren@gmail.com>
2020-01-09 13:44:50 +0100

    Changed text

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace azuredemo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World 2.0!");
                 });
             });
         }
